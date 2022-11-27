619a201dcd8e66158fa7fe230d244dd86210683b
Attstar2021 <attiyarasheed923@yahoo.co.uk>
2022-11-27 22:26:13 +0000

    added basic stucture to files and folders

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import NavBar from './components/NavBar';
+import styles from './App.module.css';
+import Container from 'react-bootstrap/Container';
+import {
+  Switch,
+  Route
+} from "react-router-dom";
+
+function App() {
+  return (
+    <div className={styles.App}>
+      <NavBar/>
+      <Container className={styles.Main}>
+        <Switch>
+          <Route exact path="/" render={() => <h1>Home page</h1>} />
+          <Route exact path="/signup" render={() => <h1>Sign Up</h1>} />
+          <Route exact path="/signin" render={() => <h1>Sign In</h1>} />
+          <Route render={() => <p>Page not found!</p> } />
+        </Switch>
+      </Container>
+    </div>
+  );
+}
+
+export default App;
