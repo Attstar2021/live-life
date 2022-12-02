2ee01bfcfe798d6b2ca5385ef9314d931f29f44c
Attstar2021 <attiyarasheed923@yahoo.co.uk>
2022-12-02 02:50:41 +0000

    done correction on code

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,22 +1,17 @@
 import React from "react";
 import styles from "../styles/Footer.module.css";
-import { NavLink } from "react-router-dom";
-import Container from "react-bootstrap/Container";
+
 
 const Footer = () => {
   return (
     <Container className={styles.Social} fluid align-text-bottom>
-		<NavLink
-			to="https://www.facebook.com">
-        	<i class="fab fa-facebook"></i>
-     	</NavLink>
-		<NavLink
-			to="https://www.twitter.com">
-        	<i className="fab fa-twitter"></i>
-     	</NavLink>
-		<NavLink to="https://www.instagram.com">
-        	<i className="fab fa-instagram"></i>
-     	</NavLink>
+		<MDBBtn style={{ backgroundColor: '#55acee' }} href="https://www.facebook.com">
+        <MDBIcon className='me-2' fab icon="facebook" /> Facebook
+    	</MDBBtn> <MDBBtn style={{ backgroundColor: '#55acee' }} href="https://www.twitter.com">>
+      	<MDBIcon className='me-2' fab icon="twitter" /> Twitter
+    	</MDBBtn> <MDBBtn style={{ backgroundColor: '#55acee' }} href="https://www.instagram.com">
+      	<MDBIcon className='me-2' fab icon="instagram" /> Instagram
+    	</MDBBtn>
 	</Container>
   );
 };
