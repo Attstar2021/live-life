723ba292fa6b86ee7aa71e3211872624dc62bb2d
Attstar2021 <attiyarasheed923@yahoo.co.uk>
2023-01-02 20:56:59 +0000

    debug

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "https://8000-attstar2021-livelifedrf-j8gg6lqmwpu.ws-eu79.gitpod.io/"
+axios.defaults.baseURL = "https://live-life-api.herokuapp.com/"
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
 
