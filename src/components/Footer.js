eb11ee0938a847c59de934546414b3654c80934f
Attstar2021 <attiyarasheed923@yahoo.co.uk>
2023-01-05 22:32:54 +0000

    debug

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,7 +12,9 @@ const Footer = () => {
 	<div className={styles.Footer}>
     <Container className={styles.Social} fluid align-text-bottom="True">
 		<Button href="https://www.facebook.com" target="_blank" aria-label="Find us on Facebook (link opens in a new tab)"><FaFacebook color="#ffffff"/></Button>
+		{' '}
 		<Button href="https://www.instagram.com" target="_blank" aria-label="Find us on Instagram (link opens in a new tab)">< FaInstagram color="#00ace"/></Button>
+		{' '}
 		<Button href="https://www.twitter.com" target="_blank" aria-label="Find us on Twitter (link opens in a new tab)"><FaTwitter color="#00ace"/></Button>
 	</Container>
 	</div>
